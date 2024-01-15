81c8bf1afca14e44eeacd0929cd5c6c1bb1bf3dc
Adi892 <adiyo111bm@gmail.com>
2024-01-15 14:45:58 +0530

    commit

---
+++ context/constants.js
@@ -0,0 +1,7 @@
+//organizerAddress = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'-local polygon-(0x2286e1947d1Ef63F78abFcc55a4C497152657670)
+
+import voting from "./Create.json";
+
+// export const VotingAddress = '0x2286e1947d1Ef63F78abFcc55a4C497152657670';  // for deployed
+export const VotingAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+export const VotingAddressABI = voting.abi;
